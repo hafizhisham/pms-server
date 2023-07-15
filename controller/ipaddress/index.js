a42319c1c67b381983668f39b6b722a9603ecb67
Hafiz Hisham <hafizhisham304@gmail.com>
2023-07-15 14:29:39 +0800

    update

--- controller/ipaddress/index.js
+++ controller/ipaddress/index.js
@@ -1,23 +1,24 @@
-const axios = require('axios');
+import axios from "axios";
 
+// const axios = require("axios");
 
-const ipaddress = async (req, res) => {
-const options = {
-  method: 'GET',
-  url: 'https://weatherapi-com.p.rapidapi.com/ip.json',
-  params: {q: '<REQUIRED>'},
-  headers: {
-    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+const ipaddress = async () => {
+  const options = {
+    method: "GET",
+    url: "https://weatherapi-com.p.rapidapi.com/ip.json",
+    params: { q: "<REQUIRED>" },
+    headers: {
+      "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
   }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
 };
 
 export default ipaddress;
