d49373bd303bef59d5d29d74c5e6f4e15ab9b703
Hafiz Hisham <hafizhisham304@gmail.com>
2023-07-15 15:41:35 +0800

    update

--- controller/ipaddress/index.js
+++ controller/ipaddress/index.js
@@ -3,22 +3,22 @@ import axios from "axios";
 // const axios = require("axios");
 
 const ipaddress = async () => {
-  const options = {
-    method: "GET",
-    url: "https://weatherapi-com.p.rapidapi.com/ip.json",
-    params: { q: "<REQUIRED>" },
-    headers: {
-      "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-    },
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
+    const options = {
+        method: 'GET',
+        url: 'https://weatherapi-com.p.rapidapi.com/timezone.json',
+        params: {q: 'q=Malaysia'},
+        headers: {
+          'X-RapidAPI-Key': '4d04074351mshc4f60fdd497cfa0p1f2276jsn7d8d0a60cec7',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+      };
+      
+      try {
+          const response = await axios.request(options);
+          console.log(response.data);
+      } catch (error) {
+          console.error(error);
+      }
 };
 
 export default ipaddress;
