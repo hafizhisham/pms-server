461ba11ddeec7d07255e28d45c344c0cb09af8ed
Hafiz Hisham <hafizhisham304@gmail.com>
2023-07-15 17:54:43 +0800

    update

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -32,7 +32,7 @@ const login = async (req, res) => {
           // name: user.name,
           username: user.username,
           // email: user.email,
-          // isAdmin: user.is_admin,
+          isAdmin: user.is_admin,
         });
 
         const serverRes = {
